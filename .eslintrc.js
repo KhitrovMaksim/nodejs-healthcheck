a6724128720e8d9639ca59bca33a9f7e8a01f1cd
Maksim <khitrov.maks@gmail.com>
2023-02-03 01:22:07 +0400

    Added solution for all tasks

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,9 @@ module.exports = {
     es2021: true,
   },
   extends: 'airbnb-base',
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  rules: {
-  },
+  rules: {},
 };
